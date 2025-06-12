bc07db79cc1e6715505fe466dbaf886701e33266
Jang Hyun <jhyunn06@gmail.com>
2025-06-13 00:38:22 +0700

    fix db

--- services/database/db.js
+++ services/database/db.js
@@ -1,12 +1,23 @@
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
+//
+// const pool = new Pool({
+//   user: "janghyun",
+//   password: "",
+//   host: "localhost",
+//   port: 5432,
+//   database: "webprofile"
+// });
+
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: "janghyun",
-  password: "",
-  host: "localhost",
-  port: 5432,
-  database: "webprofile"
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // untuk Railway
+  },
 });
+
 
 module.exports = pool;
 
