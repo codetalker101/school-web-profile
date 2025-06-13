93aa08f011db50ad9904caeb59af826de3839edd
Jang Hyun <jhyunn06@gmail.com>
2025-06-13 10:49:40 +0700

    fixing kepsek image on homepage

--- services/database/db.js
+++ services/database/db.js
@@ -1,22 +1,22 @@
-// const Pool = require('pg').Pool
-//
-// const pool = new Pool({
-//   user: "janghyun",
-//   password: "",
-//   host: "localhost",
-//   port: 5432,
-//   database: "webprofile"
-// });
-
-const { Pool } = require('pg');
-require('dotenv').config();
+const Pool = require('pg').Pool
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // untuk Railway
-  },
+  user: "janghyun",
+  password: "",
+  host: "localhost",
+  port: 5432,
+  database: "webprofile"
 });
+
+// const { Pool } = require('pg');
+// require('dotenv').config();
+//
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false, // untuk Railway
+//   },
+// });
 
 
 module.exports = pool;
