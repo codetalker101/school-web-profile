d44dc0477edcd8e953fa9a74eae285e411d4981a
Jang Hyun <jhyunn06@gmail.com>
2025-06-15 21:43:57 +0700

    edit db for railway

--- services/database/db.js
+++ services/database/db.js
@@ -1,26 +1,12 @@
-const Pool = require('pg').Pool
-const dotenv = require('dotenv')
-
-// path to config.env
-dotenv.config({ path: 'config.env' });
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  host: "localhost",
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // untuk Railway
+  },
 });
-
-// const { Pool } = require('pg');
-// require('dotenv').config();
-//
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false, // untuk Railway
-//   },
-// });
 
 
 module.exports = pool;
