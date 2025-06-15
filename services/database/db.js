aa6eb8467d5541573bb3b8c6a7e5fb819256d5d9
Jang Hyun <jhyunn06@gmail.com>
2025-06-15 21:21:56 +0700

    Remove backup.sql from repo and ignore it

--- services/database/db.js
+++ services/database/db.js
@@ -1,12 +1,26 @@
-const { Pool } = require('pg');
-require('dotenv').config();
+const Pool = require('pg').Pool
+const dotenv = require('dotenv')
+
+// path to config.env
+dotenv.config({ path: 'config.env' });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // untuk Railway
-  },
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  host: "localhost",
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME
 });
+
+// const { Pool } = require('pg');
+// require('dotenv').config();
+//
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false, // untuk Railway
+//   },
+// });
 
 
 module.exports = pool;
