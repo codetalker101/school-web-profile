f8a29f17d4a44b64d7cf7adc02529acce7bf4425
Jang Hyun <jhyunn06@gmail.com>
2025-07-08 12:42:40 +0700

    commit for hosting

--- services/database/db.js
+++ services/database/db.js
@@ -1,3 +1,17 @@
+// const Pool = require('pg').Pool
+// const dotenv = require('dotenv')
+//
+// // path to config.env
+// dotenv.config({ path: 'config.env' });
+//
+// const pool = new Pool({
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   host: "localhost",
+//   port: process.env.DATABASE_PORT,
+//   database: process.env.DATABASE_NAME
+// });
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
