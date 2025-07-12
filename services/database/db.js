55200168814c8885424624b560a573d1f9a9a8af
Jang Hyun <jhyunn06@gmail.com>
2025-07-12 21:45:48 +0700

    add db connection logging

--- services/database/db.js
+++ services/database/db.js
@@ -1,3 +1,17 @@
+// const Pool = require('pg').Pool
+// const dotenv = require('dotenv')
+//
+// // path to config.env
+// dotenv.config({ path: 'config.env' });
+//
+// const pool = new Pool({
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   host: "localhost",
+//   port: process.env.DATABASE_PORT,
+//   database: process.env.DATABASE_NAME
+// });
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -6,6 +20,9 @@ const pool = new Pool({
   ssl: {
     rejectUnauthorized: false,
   },
+  pool.connect()
+  .then(() => console.log("✅ Connected to PostgreSQL database on Railway"))
+  .catch(err => console.error("❌ Could not connect to PostgreSQL:", err));
 });
 
 
