24c679368cda34b9085288ce1c171d6151394cff
Jang Hyun <jhyunn06@gmail.com>
2025-07-12 19:37:41 +0700

    fix port syntax

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ dotenv.config({ path: 'config.env' });
 // listening to port
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, '0.0.0.0' () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server connected to port ${PORT}`)
 })
 
